f40ae0d3fd71798701b0d75ffb62c842e0830d2d
Serign Jobe <serignjobe@gmail.com>
2015-01-28 16:19:03 -0500

    Update scrape_wikipedia_page.js
    Update the scraping code.

--- scrape_wikipedia_page.js
+++ scrape_wikipedia_page.js
@@ -4,7 +4,7 @@ var records = [];
 $('table:first tbody tr').each(function(i, tr) {
    var kids = $(tr).children().toArray();
   records.push({
-    type: $('div', kids[0]).attr('id'),
+    type: $.trim($(kids[0]).text()),
     decimal_value: parseInt($(kids[1]).text()),
     rfc: $(kids[2]).text(),
     description: $(kids[3]).text(),
